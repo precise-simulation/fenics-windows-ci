7567b71bc1877425d1319c0cb341949b149f0a93
Precise Simulation <18078865+precsim@users.noreply.github.com>
2026-08-25 20:46:52 +0700

    dolfinx: use stable Windows FEM smoke test

--- recipes/dolfinx/test-dolfinx.bat
+++ recipes/dolfinx/test-dolfinx.bat
@@ -54,7 +54,7 @@ cd python/demo
 :: run some tests
 cd ../test
 :: subset of tests should exercise dependencies, solvers, partitioners
-set "TESTS=unit/fem/test_fem_pipeline.py unit/mesh/test_mesh_partitioners.py"
+set "TESTS=unit/fem/test_fem_pipeline.py::test_P_simplex unit/mesh/test_mesh_partitioners.py"
 pytest -v -m "not petsc4py and not adios2" %TESTS%
 if errorlevel 1 exit 1
 
